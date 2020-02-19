738a11993ab829975927cace3eeb744108c27ae5
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-02-19 21:46:21 +0500

    Initial Commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Алексей Пархоменко on 19.02.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cenliusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+}
+
